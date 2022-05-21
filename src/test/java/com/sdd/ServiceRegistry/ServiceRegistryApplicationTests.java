4f2ff0f9f6043f46da3e5e6812dfa8856d88b009
vivekindia <vivek031099@gmail.com>
2022-05-21 22:11:41 +0530

    Service Registry Setup

---
+++ src/test/java/com/sdd/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdd.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
